361a641931278b99776037ba40689f3d59cb2f81
Talha Iqbal <talhaiqbal801@gamil.com>
2019-07-07 17:05:14 +0500

    Add GET /users/me route and authentication middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 require('./config/config');
+require('./db/mongoose');
 
 const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 app.use(bodyParser.json());
@@ -93,13 +94,26 @@ app.post('/users', (req, res) => {
 
     var user = new User(body);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
-        res.header('x-auth', token).send(user);
-    }).catch(e => {
-        res.status(400).send(e);
+    user.save(async function() {
+        try{
+            var token = await user.generateAuthToken();
+            res.header('x-auth', token).send(user);
+        } catch(e) {
+            res.status(400).send(e);
+        }
     });
+
+    // user.save().then(() => {
+    //     return user.generateAuthToken();
+    // }).then((token) => {
+    //     res.header('x-auth', token).send(user);
+    // }).catch(e => {
+    //     res.status(400).send(e);
+    // });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
